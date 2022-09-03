aa78cfed89c052a0a0deeda33470f6451e8e52c7
uhururawlings40 <uhururawlings40@gmail.com>
2022-09-03 21:09:23 +0300

    added students login page

--- Auth/index.php
+++ Auth/index.php
@@ -4,10 +4,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/authentication.php">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/authentication.css">
+    <link rel="stylesheet" href="../css/students.css">
     <title>A.M.S | Login</title>
 </head>
 <body>
-    
+    <div class="shadow_login_sections">
+        <div class="login_sections">
+            <h3>Attendance Management System</h3>
+            <p>Please provide the following details to login</p>
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="admissionnumber">Adm No.</label>
+                    <input type="text" name="admissionnumber" id="admissionnumber" class="form-control" placeholder="Adm No.">
+                </div>
+                <div class="form-group">
+                    <label for="passwords">Password</label>
+                    <input type="password" name="passwords" id="passwords" class="form-control" placeholder="Adm No.">
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="login" id="login" class="btn btn-primary w-100" value="Login">
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
